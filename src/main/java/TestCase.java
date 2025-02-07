656849f9f999b324c13f0154893bcaf92e0a5162
arifrazak8 <arif.raza@weavers-web.com>
2025-02-07 19:52:28 +0530

    New

--- src/main/java/TestCase.java
+++ src/main/java/TestCase.java
@@ -1,4 +1,6 @@
 import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -11,9 +13,20 @@ public class TestCase extends Launch {
         try {
             new Clock_In().Clock_in();
         } catch (Exception e) {
-            System.out.println("Clock_in not reguired "+ e.getMessage());
+            System.out.println("Clock_in is not required ");
+            var Menu = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.ImageView\").instance(2)"));
+            Menu.click();
+            var Dispatch_Form = driver.findElement(AppiumBy.accessibilityId("Driver Dispatch Form"));
+            Dispatch_Form.click();
         }
-
+        var el3 = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().className(\"android.view.View\").instance(15)"));
+        el3.click();
+        var el5 = driver.findElement(AppiumBy.className("android.widget.EditText"));
+        el5.sendKeys("7004");
+        var el6 = driver.findElement(AppiumBy.accessibilityId("7004-Myntra Warehouse-(terminal)"));
+        wait.until(ExpectedConditions.elementToBeClickable(el6));
+        el6.click();
+        Thread.sleep(5000);
     }
 
 }
